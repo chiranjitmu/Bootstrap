ae2123af30d8bc68ac77d9c7386446af44716f8d
Chiranjitprojects <Chiranjitmukherjee2002@gmail.com>
2023-04-21 16:52:34 +0530

    second commit

--- src/Headerpic.js
+++ src/Headerpic.js
@@ -5,7 +5,7 @@ function Headerpic() {
   return (
     <div>
         <img className='header_image' src='./Laptop.jpg' alt=''></img>
-        <p className='title_image'>Computer Engineering</p>
+        <p className='title_image'>Computer Enginering</p>
         <p className='title2_image'>142,765 Computer Engineers follow this</p>
     </div>
   )
